88382c4ce1da89909292be11d20666e977ba0e8e
Clément Hélardot <clement.helardot@gmail.com>
2020-11-27 18:54:54 +0100

    removed unused tailwind rules, added firebase config for prod, added webpack-bundle-analyzer package

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyD32pvDz2e70r6Vox5Xlemu6yjm9y5Tugo',
+    authDomain: 'gogomedia-dashboard.firebaseapp.com',
+    databaseURL: 'https://gogomedia-dashboard.firebaseio.com',
+    projectId: 'gogomedia-dashboard',
+    storageBucket: 'gogomedia-dashboard.appspot.com',
+    messagingSenderId: '835414133389',
+    appId: '1:835414133389:web:c3a9a2d336fcc2f6e270c4',
+    measurementId: 'G-X38MEP8EK7',
+  },
 };
